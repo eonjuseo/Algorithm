588380f49f4a25bc6f21a58222947935c402c7be
Seo Eonju <72714809+eonjuseo@users.noreply.github.com>
2024-10-02 23:55:14 +0900

    [level 2] Title: JadenCase 문자열 만들기, Time: 4.00 ms, Memory: 79.3 MB -BaekjoonHub

---
+++ 프로그래머스/2/12951. JadenCase 문자열 만들기/JadenCase 문자열 만들기.java
@@ -0,0 +1,23 @@
+import java.util.*;
+class Solution {
+    public String solution(String s) {
+        String answer = "";
+        String[] arr = s.split(" ");
+
+        for(int i = 0; i < arr.length; i++) {
+            if(arr[i].length() == 0) {
+                answer += " ";
+            }
+            else {
+                answer += arr[i].substring(0, 1).toUpperCase(); 
+                answer += arr[i].substring(1, arr[i].length()).toLowerCase(); 
+                answer += " "; 
+            }
+        }
+        // 입력 받은 문자열의 맨 마지막이 " " 라면 바로 answer 반환
+        if(s.substring(s.length() -1, s.length()).equals(" ")) return answer;
+        // 맨 마지막 " " 제거하고 answer 반환
+        return answer.substring(0, answer.length() - 1);
+        
+    }
+}
